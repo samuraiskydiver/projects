7288160ec27125b4328870aae656d33d308c3a33
samuraiskydiver <67636416+samuraiskydiver@users.noreply.github.com>
2021-03-23 01:08:28 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+API_KEY = 'PKR4JTRP5HE952PJCF4D'
+API_SECRET = 'oH0kq6LvKqyCRet3nNms2ozlbRUbNnBLcmFrukms'
+
+WEBHOOK_PASSPHRASE = 'kvostov'
+#DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/823656221886251040/IMn7WMwroroUQ1fG0Nn6J26vxhkt36rYKnp2n3P61wYagvKvlQnMyuz4Vjn0wgO2rmu7' # use a string containing your discord webhook url to enable
